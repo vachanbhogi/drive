ef8777f827db1956be73938b83eb97e3f679636f
Vachan Bhogi <vachanbhogi@outlook.com>
2024-04-06 22:31:00 -0700

    optimize track

--- Track/track.gd
+++ Track/track.gd
@@ -1,23 +1,31 @@
-extends Path3D
+extends Node
 
-var points = PackedVector3Array()
-var timer = Timer.new()
+@export var player: RigidBody3D
+
 var direction = Vector3.FORWARD
+const MAX_POINTS = 5
+const ADD_THRESHOLD_SQUARED = 15000
+const BATCH_SIZE = 3
 
-func _ready():
-	add_child(timer)
-	timer.wait_time = 1.75
-	timer.connect("timeout", Callable(self, "_on_Timer_timeout"))
-	timer.start()
+@onready var path: Path3D = $Path
 
-func _on_Timer_timeout():
-	if curve.get_point_count() > 7:
-		curve.remove_point(0) 
+func _physics_process(_delta: float) -> void:
+	var lastPointCount = path.curve.get_point_count()
+	if player.global_position.distance_squared_to(path.curve.get_point_position(lastPointCount - 1)) < ADD_THRESHOLD_SQUARED:
+		_add_road()
 
-	if curve.get_point_count() > 0:
-		var last_point = curve.get_point_position(curve.get_point_count() - 1)
-		var rotation_angle = deg_to_rad(randf_range(-45, 45))
+func _add_road():
+	var excessPoints = path.curve.get_point_count() - MAX_POINTS
+	for x in range(excessPoints):
+		path.curve.remove_point(0)
+
+	var lastPoint = path.curve.get_point_position(path.curve.get_point_count() - 1)
+	for x in range(BATCH_SIZE):
+		var rotationAngle = deg_to_rad(randf_range(-45, 45))
 		var distance = randf_range(40, 60)
-		var new_point = Vector3(last_point.x + direction.x * distance, last_point.y, last_point.z + direction.z * distance)
-		curve.add_point(new_point)
-		direction = Vector3(direction.x, 0, direction.z).rotated(Vector3.UP, rotation_angle)
+		var newX = lastPoint.x + direction.x * distance
+		var newZ = lastPoint.z + direction.z * distance
+		path.curve.add_point(Vector3(newX, lastPoint.y, newZ))
+		direction = Vector3(direction.x, 0, direction.z).rotated(Vector3.UP, rotationAngle)
+		lastPoint.x = newX
+		lastPoint.z = newZ
